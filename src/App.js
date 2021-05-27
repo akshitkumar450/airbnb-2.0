1e6c92c7ba7f371c84d110b05d2c6c14bb3dfa84
Akshit Kumar <akshitkumar450@gmail.com>
2021-05-28 00:49:42 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Footer from './Footer'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import SearchPage from './SearchPage';
 
 function App() {
   return (
     <div className="app">
-      <Header />
+      <Router>
+        <Header />
 
-      <Home />
+        <Switch>
+          <Route path='/' exact={true}>
+            <Home />
+          </Route>
+          <Route path='/search' exact={true}>
+            <SearchPage />
+          </Route>
+        </Switch>
 
-      <Footer />
+        <Footer />
+      </Router>
     </div>
   );
 }
--- src/Banner.js
+++ src/Banner.js
@@ -2,9 +2,10 @@ import { Button } from '@material-ui/core'
 import React, { useState } from 'react'
 import "./Banner.css"
 import Search from './Search'
+import { Link, useHistory } from 'react-router-dom'
 
 function Banner() {
-
+    const history = useHistory()
     const [showSearch, setShowSearch] = useState(false)
 
     return (
@@ -24,7 +25,7 @@ function Banner() {
                 <h5>
                     Plan a different kind of getaway to uncover the hidden gems near you.
                 </h5>
-                <Button variant='outlined'>Explore Nearby </Button>
+                <Button variant='outlined' onClick={() => history.push('/search')} >Explore Nearby </Button>
             </div>
         </div>
     )
